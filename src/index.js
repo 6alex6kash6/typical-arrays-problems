d3f0b1e40d6d5fc28839dc1634847b2794d44acd
Alexandr <6alex6kash6@gmail.com>
2020-03-03 13:15:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    if (array.length === 0) {
+        return 0;
+    }
+    return Math.min.apply(null, array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (array.length === 0) {
+        return 0;
+    }
+    return Math.max.apply(null, array);
+};
+
+exports.avg = function avg(array) {
+    if (!Array.isArray(array)) {
+        return 0;
+    }
+
+    if (array.length === 0) {
+        return 0;
+    }
+    let result = array.reduce((sum, current) => sum + current, 0);
+    return result / array.length;
+};
